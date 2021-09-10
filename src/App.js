cd9e7253ec46325d0fdc7767805b9efcf618bab4
Nathan <nathanraysheryak@gmail.com>
2021-09-10 06:36:43 -0400

    setup react-router + welcome route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
+
 import NotePad from "./components/NotePad.js";
+import Welcome from "./components/Welcome.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <span className="title">CloudNote</span>
-        <span className="logout-link">logout</span>
-      </header>
-      <NotePad />
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="App-header">
+          <Link className="title link" to="/">
+            <span>CloudNote</span>
+          </Link>
+          <Link className="link logout-link" to="/welcome">
+            <span>logout</span>
+          </Link>
+        </nav>
+
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+
+          <Route path="/">
+            <NotePad />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>WELCOME</div>;
+  }
+}
+
+export default Welcome;
