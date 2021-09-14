f8db2bba03972f183175d85a2f952db2a9114e0a
Nathan <nathanraysheryak@gmail.com>
2021-09-14 07:20:12 -0400

    bind welcome inputs with local state

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,24 @@ import "./Welcome.css";
 class Welcome extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      password: "",
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordCange = this.handlePasswordCange.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({
+      email: event.target.value,
+    });
+  }
+  handlePasswordCange(event) {
+    this.setState({
+      password: event.target.value,
+    });
   }
 
   render() {
@@ -12,8 +29,18 @@ class Welcome extends React.Component {
       <div>
         <form className="login-container">
           <h2>Log In</h2>
-          <input placeholder="email" type="text" />
-          <input placeholder="password" type="password" />
+          <input
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+            placeholder="email"
+            type="text"
+          />
+          <input
+            value={this.state.password}
+            onChange={this.handlePasswordCange}
+            placeholder="password"
+            type="password"
+          />
           <button>Log In</button>
         </form>
       </div>
