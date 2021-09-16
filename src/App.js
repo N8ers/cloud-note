9f5ca7646f60017a80fc5bd6c00cfd0cc44b47f7
Nathan <nathanraysheryak@gmail.com>
2021-09-16 07:32:15 -0400

    create store, dummy reducers + render content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./App.css";
 
@@ -6,6 +7,8 @@ import NotePad from "./components/NotePad.js";
 import Welcome from "./components/Welcome.js";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
   return (
     <Router>
       <div className="App">
@@ -24,6 +27,12 @@ function App() {
           </Route>
 
           <Route path="/">
+            <h1>Counter: {counter}</h1>
+            <button>+</button>
+            <button>-</button>
+
+            <h1>isLogged: </h1>
+            {isLogged ? <div>yes</div> : <h1>no</h1>}
             <NotePad />
           </Route>
         </Switch>
