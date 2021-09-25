abf338ee7d71e350edf47d4e8bb673dd56f8a482
Nathan <nathanraysheryak@gmail.com>
2021-09-25 14:42:19 -0400

    create NotePreview component + css file

---
+++ src/components/NotePreview/NotePreview.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./NotePreview.css";
+
+function NotePreview() {
+  return (
+    <div className="NotePreview">
+      <h1>I am Note Title</h1>
+      <p>I am a note body that gets truncated...</p>
+    </div>
+  );
+}
+
+export default NotePreview;
