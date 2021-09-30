9087a7e3cb450c80d010f7701a02a55622dec539
Nathan <nathanraysheryak@gmail.com>
2021-09-30 07:15:43 -0400

    truncate note preview body + add border styling

--- src/components/NotePreview/NotePreview.js
+++ src/components/NotePreview/NotePreview.js
@@ -3,10 +3,14 @@ import React from "react";
 import "./NotePreview.css";
 
 function NotePreview(props) {
+  function truncateText(text) {
+    return text.substring(0, 36) + "...";
+  }
+
   return (
     <div className="NotePreview">
       <h1>{props.title}</h1>
-      <p>{props.body}</p>
+      <p>{truncateText(props.body)}</p>
     </div>
   );
 }
