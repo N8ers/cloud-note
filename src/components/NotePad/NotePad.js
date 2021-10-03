c759100965f6464ff6618d8b8077ff44b8b19e25
Nathan <nathanraysheryak@gmail.com>
2021-10-03 07:12:05 -0400

    bind note title to state

--- src/components/NotePad/NotePad.js
+++ src/components/NotePad/NotePad.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { updateNote } from "./notePadSlice";
+import { updateNoteBody, updateNoteTitle } from "./notePadSlice";
 
 import "./NotePad.css";
 
 function NotePad() {
   const note = useSelector((state) => state.notePad.value);
+  const title = useSelector((state) => state.notePad.title);
   const dispatch = useDispatch();
 
   return (
     <div className="NotePad">
-      <input value="I am the Note Title" />
+      <input
+        value={title}
+        onChange={(event) => dispatch(updateNoteTitle(event.target.value))}
+      />
       <textarea
         value={note}
-        onChange={(event) => dispatch(updateNote(event.target.value))}
+        onChange={(event) => dispatch(updateNoteBody(event.target.value))}
       ></textarea>
     </div>
   );
--- src/components/NotePad/notePadSlice.js
+++ src/components/NotePad/notePadSlice.js
@@ -2,19 +2,24 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const lorem =
   "I'm baby four dollar toast vaporware sartorial coloring book organic. Banjo yuccie post-ironic tumeric. Forage kickstarter echo park, schlitz bicycle rights humblebrag keffiyeh live-edge kombucha hoodie raw denim poutine pug knausgaard food truck. Typewriter cloud bread austin gluten-free VHS actually PBR&B live-edge. Roof party austin edison bulb banh mi williamsburg chia biodiesel authentic. Church-key lyft distillery artisan migas vaporware XOXO mustache. Photo booth kitsch cardigan fanny pack, gluten-free flexitarian microdosing etsy YOLO selvage.";
+const testTitle = "To hip or not to ip";
 
 export const notePadSlice = createSlice({
   name: "notePad",
   initialState: {
     value: lorem,
+    title: testTitle,
   },
   reducers: {
-    updateNote: (state, action) => {
+    updateNoteBody: (state, action) => {
       state.value = action.payload;
+    },
+    updateNoteTitle: (state, action) => {
+      state.title = action.payload;
     },
   },
 });
 
-export const { updateNote } = notePadSlice.actions;
+export const { updateNoteBody, updateNoteTitle } = notePadSlice.actions;
 
 export default notePadSlice.reducer;
