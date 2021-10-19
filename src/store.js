8ffea9d75b3af75d1e1b1a74a3632116e7f606ad
Nathan <nathanraysheryak@gmail.com>
2021-10-19 07:15:49 -0400

    create apiSlice + add to store

--- src/store.js
+++ src/store.js
@@ -2,11 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import notePadReducer from "./components/NotePad/notePadSlice";
 import welcomeReducer from "./components/Welcome/welcomeSlice";
 import notesReducer from "./components/Notes/notesSlice";
+import { apiSlice } from "./components/api/apiSlice";
 
 export default configureStore({
   reducer: {
     notePad: notePadReducer,
     welcome: welcomeReducer,
     notes: notesReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
