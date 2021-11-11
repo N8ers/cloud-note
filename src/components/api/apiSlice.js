223d52c3e26af6f57be2298d61862f0615f5f10b
Nathan <nathanraysheryak@gmail.com>
2021-11-11 07:20:03 -0500

    start building post into slice

--- src/components/api/apiSlice.js
+++ src/components/api/apiSlice.js
@@ -1,12 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const defaultUserId = 4;
+
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000" }),
-  endpoints: (builder) => ({
-    getNotes: builder.query({
+  endpoints: (build) => ({
+    getNotes: build.query({
       // baseUrl + '/notes'
       query: () => "/notes/slow",
+    }),
+    addNote: build.mutation({
+      query: (body) => ({
+        url: `posts`,
+        method: "POST",
+        body,
+      }),
     }),
   }),
 });
