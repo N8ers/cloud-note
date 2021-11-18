0aa317a6c84fd87a139e306a3e062d30534db1ea
Nathan <nathanraysheryak@gmail.com>
2021-11-18 07:16:22 -0500

    start styling NewNote

--- src/components/NewNote/NewNote.js
+++ src/components/NewNote/NewNote.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 
+import "./NewNote.css";
+
 const NewNote = (props) => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
   return (
-    <div>
+    <div className="NotePad">
       <button>save</button>
       <button>cancel</button>
       <input value={title} onChange={(event) => setTitle(event.target.value)} />
