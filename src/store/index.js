16772e079a5ab720a8940ef6b15ec07aec2d4928
Nathan <nathanraysheryak@gmail.com>
2021-11-29 07:36:40 -0500

    start replacing rtk w/ vanilla redux

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,28 @@ import redux from "redux";
 
 // default state
 const defaultState = {
+  user: {
+    email: "tsuki@meow.mail",
+    password: "pstpstpstpst",
+  },
   notes: [{ id: 1, title: "throw rock", body: "today i need to throw a rock" }],
 };
 
 // reducers
 const notesReducer = (state = defaultState, action) => {
+  if (action.type === "login") {
+  }
+  if (action.type === "logout") {
+    return {
+      state: {
+        ...state.notes,
+        ...state.user,
+        // state.user.email: '',
+        // state.user.password: ''
+      },
+    };
+  }
+
   if (action.type === "createNote") {
     return {
       state: {
